7a8583d3860332e09a6b541dfd7dc27e5657ef22
pranjalsidh905 <sidhpranjal905@gmail.com>
2025-06-24 12:13:23 +0530

    server file update

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-const photoRoutes = require('./routes/photoRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
-const userRoutes = require('./routes/userRoutes');
+const photoRoutes = require("./routes/photoRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 // MIDDLEWARES
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(express.json({ limit: '50mb' }));
-app.use(cors({ origin: '*', methods: '*' }));
-
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  cors({
+    origin: "https://photo-sharing-gallery.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // ROUTES
-app.use('/photos', photoRoutes);
-app.use('/categories', categoryRoutes);
-app.use('/users', userRoutes);
+app.use("/photos", photoRoutes);
+app.use("/categories", categoryRoutes);
+app.use("/users", userRoutes);
 
-mongoose.connect(process.env.MONGODB_URL)
-    .then(console.log("Successfully connected to database"));
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(console.log("Successfully connected to database"));
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
